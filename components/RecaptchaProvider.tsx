f0aaab80a2c052d36b650ba3673d31a9fdaf3257
nelius72 <cornelioromeroborrero@gmail.com>
2026-05-11 20:34:07 +0200

    Fix Anytype

--- components/RecaptchaProvider.tsx
+++ components/RecaptchaProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
-export default function ReCaptchaProvider({ children }) {
+export default function ReCaptchaProvider({ children }: { children: React.ReactNode }) {
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? ""}
